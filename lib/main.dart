65d9577144420e3223162fef485e6ae79ba41827
Kesavan-Hex <kesavang4020@gmail.com>
2023-09-27 22:14:32 +0530

    Business information segmation UI Done

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,9 @@ class _MyHomePageState extends State<MyHomePage> {
     'Business Verification',
   ];
 
+  // Dropdown value
+  String? _selectedBusinessType;
+
   @override
   void dispose() {
     businessNameController.dispose();
@@ -107,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: <Widget>[
                     GestureDetector(
                       onTap: () {
-                        // Handle back action based on segment need to be updated
+                        // Handle back button press here
                       },
                       child: Container(
                         width: 25,
@@ -196,10 +199,20 @@ class _MyHomePageState extends State<MyHomePage> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Text(
+                            'General Information',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.black,
+                              fontFamily: 'DM Sans',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
                             'Enter your business details',
                             style: TextStyle(
                               fontSize: 16,
-                              color: Colors.black,
+                              color: Colors.grey,
                             ),
                           ),
                           SizedBox(height: 18),
@@ -225,7 +238,7 @@ class _MyHomePageState extends State<MyHomePage> {
                               controller: webURLController,
                               decoration: InputDecoration(
                                 labelText: 'Web URL',
-                                hintText: 'burgerking.com',
+                                hintText: 'sourcecoders.in',
                                 border: OutlineInputBorder(),
                                 errorText: webURLError,
                               ),
@@ -272,6 +285,99 @@ class _MyHomePageState extends State<MyHomePage> {
                           ),
                         ],
                       ),
+                    // Inside the Content for Business Information segment
+                    // Inside the Content for Business Information segment
+                    if (_currentSegmentIndex == 1)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Display Business Information',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.black,
+                              fontFamily: 'DM Sans',
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 20), // Reduce space
+                          Text(
+                            'Select your business type and mark location.',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey,
+                              fontFamily: 'DM Sans',
+                            ),
+                          ),
+                          SizedBox(height: 20), // Reduce space
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Color.fromARGB(255, 126, 126, 126),
+                                  width: 2), // Border
+                              borderRadius:
+                                  BorderRadius.circular(8), // Rounded corners
+                            ),
+                            child: DropdownButton<String>(
+                              items: [
+                                DropdownMenuItem<String>(
+                                  value: 'Family Owned Business',
+                                  child: Text('Family Owned Business'),
+                                ),
+                                DropdownMenuItem<String>(
+                                  value: 'Partnership',
+                                  child: Text('Partnership'),
+                                ),
+                                DropdownMenuItem<String>(
+                                  value: 'Solo Business',
+                                  child: Text('Solo Business'),
+                                ),
+                              ],
+                              onChanged: (String? newValue) {
+                                // Handle dropdown value change here
+                              },
+                              hint: Text('Select your business type'),
+                              isExpanded: true,
+                              underline: Container(), // Remove the underline
+                            ),
+                          ),
+                          SizedBox(height: 25), // Reduce space
+                          ElevatedButton(
+                            onPressed: () {
+                              // Handle the button press here
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Color(0xFF0085FF), // Button color
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 24,
+                                  ),
+                                  SizedBox(width: 8), // Add space
+                                  Text(
+                                    'Add Location',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                    SizedBox(height: 20),
                   ],
                 ),
               ),
